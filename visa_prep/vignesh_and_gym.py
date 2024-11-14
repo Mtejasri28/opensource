f03dc15ecff84c497b3627ff3c16e070390c68d4
Mtejasri28 <163152730+Mtejasri28@users.noreply.github.com>
2024-11-14 20:22:07 +0530

    Update vignesh_and_gym.py

--- visa_prep/vignesh_and_gym.py
+++ visa_prep/vignesh_and_gym.py
@@ -1,8 +1,9 @@
 x,y,z=map(int,input().split())
 total= x + y
-if total>=z:
-    print("2")
-elif total<z:
-    print("1")
-elif z<x:
+if z<x:
     print("0")
+elif total>z:
+    print("1")
+else:
+    print("2")
+
