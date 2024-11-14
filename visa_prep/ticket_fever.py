356fb092ea1fd8519fdf478e230af81ff2938900
Mtejasri28 <163152730+Mtejasri28@users.noreply.github.com>
2024-11-14 20:58:12 +0530

    Create ticket_fever.py

---
+++ visa_prep/ticket_fever.py
@@ -0,0 +1,8 @@
+n=int(input())
+for i in range(0,n):
+    y,z=map(int,input().split())
+    if y>z:
+        print(y-z)
+    else:
+        print("0")
+                                                                                                                                  
